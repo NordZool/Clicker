ce9ad6bc8f3c7d088b253ddf678e95f3b161d13b
Nikita <volodko.nikita.1979@gmail.com>
2024-04-02 16:57:03 +0300

    Create and integrate "ColorItem", start to make "ColorEditView"

---
+++ Clicker/Color/ColorView.swift
@@ -0,0 +1,18 @@
+//
+//  ColorView.swift
+//  Clicker
+//
+//  Created by admin on 2.04.24.
+//
+
+import SwiftUI
+
+struct ColorView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorView()
+}
---
+++ Clicker/Grid/EditViews/ColorEditView.swift
@@ -0,0 +1,18 @@
+//
+//  ColorEditView.swift
+//  Clicker
+//
+//  Created by admin on 2.04.24.
+//
+
+import SwiftUI
+
+struct ColorEditView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ColorEditView()
+}
