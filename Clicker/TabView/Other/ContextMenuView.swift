bd30ce8e6069ff1743a01b20c97c4c7720ffd382
Nikita <volodko.nikita.1979@gmail.com>
2024-04-13 18:21:32 +0300

    Add exit button for "EditView". Create "ActiveClickerTableView" and built first-setup. Isolate view that was been in .contextMenu in "GridView" as "ContextMenuView". Done a little changes in other files.

---
+++ Clicker/TabView/Other/ContextMenuView.swift
@@ -0,0 +1,18 @@
+//
+//  ContextMenuView.swift
+//  Clicker
+//
+//  Created by admin on 13.04.24.
+//
+
+import SwiftUI
+
+struct ContextMenuView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ContextMenuView()
+}
