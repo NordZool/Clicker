573aba8cb0d806ab52c1d40723ba43c628e9699e
Nikita <volodko.nikita.1979@gmail.com>
2024-04-19 17:22:44 +0300

    Developed main idea of the project, involve some little improvements for views. Signed "ScreenEnum" as "CaseIterable" for update ClickerTabViewBar and made it dynamic ("CaseIterable" also will be usefull in a future, when would involved the TabView in "ContentView")

---
+++ Clicker/TabView/ClickerTabViewBar.swift
@@ -0,0 +1,43 @@
+//
+//  ClickerTabView.swift
+//  Clicker
+//
+//  Created by admin on 28.02.24.
+//
+
+import SwiftUI
+
+struct ClickerTabViewBar: View {
+    @Binding var screenInView: ScreensEnum
+    
+    var body: some View {
+            HStack {
+                Spacer()
+                ForEach(ScreensEnum.allCases, id:\.self) {screenEnumCase in
+                    Button {
+                        withAnimation(.easeOut) {
+                            screenInView = screenEnumCase
+                        }
+                    } label: {
+                        Image(systemName: screenEnumCase.rawValue)
+                    }
+                    .tag(screenEnumCase)
+                    .foregroundStyle(screenInView == screenEnumCase ? Color.primary : .secondary)
+                    .scaleEffect(1.5)
+                    Spacer()
+                }
+            }
+    }
+}
+
+//only for preview
+fileprivate struct PreviewClickerTabView : View {
+    @State private var screenInView = ScreensEnum.clickers
+    var body: some View {
+        ClickerTabViewBar(screenInView: $screenInView)
+    }
+}
+
+#Preview {
+    PreviewClickerTabView()
+}
