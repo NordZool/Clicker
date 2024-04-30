0ac0a3e84d1c8b057aa0ef2977d8b5cb89d03662
Nikita <volodko.nikita.1979@gmail.com>
2024-04-30 18:37:11 +0300

    testing

--- Clicker/ClickersView/InactiveClickersTableView/InactiveClickersTableView.swift
+++ Clicker/ClickersView/InactiveClickersTableView/InactiveClickersTableView.swift
@@ -2,30 +2,17 @@
 //  InactiveClickersTableView.swift
 //  Clicker
 //
-//  Created by admin on 17.04.24.
+//  Created by admin on 30.04.24.
 //
 
 import SwiftUI
 
 struct InactiveClickersTableView: View {
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Clicker.timestamp, ascending: true)],
-        predicate: NSPredicate(format: "isActive == %@", NSNumber(value: false)), animation: Resourses.gridAnimation)
-    private var clickers: FetchedResults<Clicker>
-    
     var body: some View {
-        GridView(items: clickers.map({$0}), onItemTap: { object in
-            let clicker = object as! Clicker
-            clicker.timestamp = .now
-            clicker.isActive = true
-            if let context = clicker.managedObjectContext {
-                try! context.save()
-            }
-        }, editMenuType: .clicker,
-                 appearAddButton: true)
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
-//#Preview {
-//    InactiveClickersTableView()
-//}
+#Preview {
+    InactiveClickersTableView()
+}
